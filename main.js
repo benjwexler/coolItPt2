324a530271620e85e0ecbc7523f614c8ba77a74d
Ben Wexler <bwexler@Bens-MacBook-Pro.local>
2018-12-22 17:53:56 -0500

    trying to decide on footer

---
+++ main.js
@@ -0,0 +1,30 @@
+document.getElementById("youtube").addEventListener("touchstart", function(){
+    console.log("hey")
+    document.getElementById("youtube").style.color = "red"
+});
+
+document.getElementById("youtube").addEventListener("touchend", function(){
+    console.log("hey")
+    document.getElementById("youtube").style.color = "black"
+});
+
+document.getElementById("spotify").addEventListener("touchstart", function(){
+    console.log("hey")
+    document.getElementById("spotify").style.color = "#1DB954"
+});
+
+document.getElementById("spotify").addEventListener("touchend", function(){
+    console.log("hey")
+    document.getElementById("spotify").style.color = "black"
+});
+
+document.getElementById("soundcloud").addEventListener("touchstart", function(){
+    console.log("hey")
+    document.getElementById("soundcloud").style.color = "#ff8800"
+});
+
+document.getElementById("soundcloud").addEventListener("touchend", function(){
+    console.log("hey")
+    document.getElementById("soundcloud").style.color = "black"
+});
+
